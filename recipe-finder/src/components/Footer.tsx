15c9a04b686acc77303678bec0c274575ea57ef4
Pablo <ortegaperaltap@gmail.com>
2025-08-08 12:49:57 -0400

    refactor Footer component layout for improved responsiveness and styling

--- recipe-finder/src/components/Footer.tsx
+++ recipe-finder/src/components/Footer.tsx
@@ -1,39 +1,40 @@
 export default function Footer() {
   return (
     <footer>
-      <div className="footer-content px-6 py-4 mt-16 flex justify-between items-center lg:px-30">
+      <div className="footer-content px-6 py-4 mt-16 flex flex-col-reverse justify-between 
+      items-start gap-4 md:flex-row lg:px-30">
         <div className="footer-company">
           <p className="text-[.9rem]">
             ©2025 <span>Recipe Finder.</span>
           </p>
         </div>
         <div className="footer-terms">
-          <a href="#" className="text-(--clr-secondary) text-[.9rem] hover:text-(--clr-primary)
-          duration-100 ease-in-out">
+          <a href="#" className="text-(--clr-secondary) text-[.9rem]
+          hover:text-(--clr-primary) duration-100 ease-in-out">
             Terms
           </a>
         </div>
         <div className="footer-docs">
-          <a href="#" className="text-(--clr-secondary) text-[.9rem] hover:text-(--clr-primary)
-          duration-100 ease-in-out">
+          <a href="#" className="text-(--clr-secondary) text-[.9rem]
+          hover:text-(--clr-primary) duration-100 ease-in-out">
             Docs
           </a>
         </div>
         <div className="footer-privacy">
-          <a href="#" className="text-(--clr-secondary) text-[.9rem] hover:text-(--clr-primary)
-          duration-100 ease-in-out">
+          <a href="#" className="text-(--clr-secondary) text-[.9rem]
+          hover:text-(--clr-primary) duration-100 ease-in-out">
             Privacy
           </a>
         </div>
         <div className="footer-contact">
-          <a href="#" className="text-(--clr-secondary) text-[.9rem] hover:text-(--clr-primary)
-          duration-100 ease-in-out">
+          <a href="#" className="text-(--clr-secondary) text-[.9rem]
+          hover:text-(--clr-primary) duration-100 ease-in-out">
             Contact
           </a>
         </div>
         <div className="footer-cookies">
-          <a href="#" className="text-(--clr-secondary) text-[.9rem] hover:text-(--clr-primary)
-          duration-100 ease-in-out">
+          <a href="#" className="text-(--clr-secondary) text-[.9rem]
+          hover:text-(--clr-primary) duration-100 ease-in-out">
             Manage cookies
           </a>
         </div>
