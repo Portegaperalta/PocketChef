848786de1d4cb014aa80f0359c65682405377a13
Pablo <ortegaperaltap@gmail.com>
2025-08-08 14:28:55 -0400

    enhance BenefitRecipeCard and StartCookButton components with improved layout and hover effects; add primary hover color to App.css

--- recipe-finder/src/components/BenefitRecipeCard.tsx
+++ recipe-finder/src/components/BenefitRecipeCard.tsx
@@ -10,29 +10,31 @@ type BenefitRecipeCardProps = {
 
 export default function BenefitRecipeCard(props: BenefitRecipeCardProps) {
   return (
-    <div className="benefit-recipe-card py-2 px-4 flex gap-4 items-center 
+    <a href="#" className=''>
+      <div className="benefit-recipe-card py-2 px-4 flex gap-4 items-center 
      bg-(--clr-white) shadow-lg rounded-lg">
-      <div className="benefit-recipe-image py-1">
-        <img src={props.recipeImageUrl}
-          alt="benefit recipe image"
-          className='w-full max-w-25 h-15 md:max-w-26 md:h-18 lg:max-w-40 lg:h-20'
-        />
-      </div>
-      <div className="benefit-recipe-info lg:basis-130 flex items-center">
-        <div className="benefit-recipe-text">
-          <h3 className='benefit-recipe-title text-(--clr-secondary) 
+        <div className="benefit-recipe-image py-1">
+          <img src={props.recipeImageUrl}
+            alt="benefit recipe image"
+            className='w-full max-w-25 h-15 md:max-w-26 md:h-18 lg:max-w-40 lg:h-20'
+          />
+        </div>
+        <div className="benefit-recipe-info lg:basis-130 flex items-center">
+          <div className="benefit-recipe-text">
+            <h3 className='benefit-recipe-title text-(--clr-secondary) 
           font-[600] mb-1 lg:text-[1.4rem] lg:font-[700]'>
-            {props.recipeName}
-          </h3>
-          <p className='text-(--clr-primary) text-[.9rem] font-[500] flex 
+              {props.recipeName}
+            </h3>
+            <p className='text-(--clr-primary) text-[.9rem] font-[500] flex 
           gap-1 items-center md:text-[1rem] lg:text-[1.1rem] '>
-            <CircleCheck />{props.recipeFeature}
-          </p>
+              <CircleCheck />{props.recipeFeature}
+            </p>
+          </div>
+        </div>
+        <div className="benefit-recipe-rating hidden lg:inline-block">
+          <RatingLabel rating={props.recipeRating} />
         </div>
       </div>
-      <div className="benefit-recipe-rating hidden lg:inline-block">
-        <RatingLabel rating={props.recipeRating} />
-      </div>
-    </div>
+    </a>
   )
 }
--- recipe-finder/src/components/ui/StartCookButton.tsx
+++ recipe-finder/src/components/ui/StartCookButton.tsx
@@ -1,7 +1,10 @@
 export default function StartCookButton() {
   return (
-    <div className="start-cook-btn bg-(--clr-primary) py-1.5 px-2 rounded-lg cursor-pointer">
-      <p className="text-(--clr-white) font-[500]">Start Cook</p>
+    <div className="start-cook-btn bg-(--clr-primary) py-1.5 px-2 rounded-lg cursor-pointer
+    hover:bg-(--clr-primary-hover) duration-200 ease-in-out">
+      <a href="#" className="text-(--clr-white) font-[500]">
+        Start Cook
+      </a>
     </div>
   )
 }
