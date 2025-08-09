713451eb9cf5ec71040c7ec02c30b6ecfdaf78a9
Pablo <ortegaperaltap@gmail.com>
2025-08-09 11:23:40 -0400

    add click event to menu button so it displays dropdown menu and hide menu when clicking link

--- recipe-finder/src/components/Header.tsx
+++ recipe-finder/src/components/Header.tsx
@@ -1,23 +1,36 @@
 import { Link } from "react-router"
+import { useLocation } from "react-router-dom"
 import AppLogo from "./AppLogo"
 import DropdownMenuButton from "./ui/DropdownMenuButton"
 import HeaderSearchBar from "./ui/HeaderSearchBar"
 import HeaderOptions from "./HeaderOptions"
-import SearchButton from "./ui/SearchButton"
 import { Bookmark, House, Search, UserRound } from "lucide-react"
+import { useState, useEffect } from "react"
 
 export default function Header() {
+
+  const [menuDisplay, setMenuDisplay] = useState(false)
+  const displayDropdownMenu = () => {
+    setMenuDisplay(!menuDisplay)
+  }
+
+  const location = useLocation()
+
+  useEffect(() => {
+    setMenuDisplay(!location)
+  }, [location])
+
   return (
     <header className="bg-(--clr-primary) px-6 py-4 relative lg:px-30">
       <div className="header-content flex flex-col md:items-center md:justify-between md:flex-row">
         <div className="header-content-left mb-2 flex flex-row items-center justify-between
         md:mb-0 md:flex-col">
-          <DropdownMenuButton />
+          <DropdownMenuButton onClick={displayDropdownMenu} />
           <AppLogo />
           <div className=""></div>
         </div>
         <div className="header-content-right">
-          <div className="header-content-search md:hidden">
+          <div className={`header-content-search ${menuDisplay ? `hidden` : `block`} md:hidden`}>
             <HeaderSearchBar />
           </div>
           <div className="header-content-options">
@@ -26,14 +39,15 @@ export default function Header() {
         </div>
       </div>
 
-      <div className="header-dropdown-menu md:hidden bg-(--clr-primary)">
+      <div className={`header-dropdown-menu py-8 px-4 w-full z-100 bg-(--clr-primary)
+        ${menuDisplay ? `block` : `hidden`} md:hidden`}>
         <nav>
-          <ul className="flex flex-col gap-6">
+          <ul className="flex flex-col gap-8">
             <li>
               <Link to="/">
                 <div className="home-link flex flex-row gap-1">
                   <House color="#FFFF" />
-                  <p className="text-(--clr-white)">Home</p>
+                  <p className="text-(--clr-white) text-[1.1rem] font-[500]">Home</p>
                 </div>
               </Link>
             </li>
@@ -41,7 +55,7 @@ export default function Header() {
               <Link to="/Search-Recipe">
                 <div className="recipe-link flex flex-row gap-1">
                   <Search color="#FFFF" />
-                  <p className="text-(--clr-white)">
+                  <p className="text-(--clr-white) text-[1.1rem] font-[500]">
                     Seach Recipes
                   </p>
                 </div>
@@ -51,7 +65,7 @@ export default function Header() {
               <Link to="">
                 <div className="saved-recipes-link flex flex-row gap-1">
                   <Bookmark color="#FFFFFF" />
-                  <p className="text-(--clr-white)">
+                  <p className="text-(--clr-white) text-[1.1rem] font-[500]">
                     Saved Recipes
                   </p>
                 </div>
@@ -61,7 +75,7 @@ export default function Header() {
               <Link to="">
                 <div className="account-link flex flex-row gap-1">
                   <UserRound color="#FFFFFF" />
-                  <p className="text-(--clr-white)">
+                  <p className="text-(--clr-white) text-[1.1rem] font-[500]">
                     Your Account
                   </p>
                 </div>
--- recipe-finder/src/components/ui/DropdownMenuButton.tsx
+++ recipe-finder/src/components/ui/DropdownMenuButton.tsx
@@ -1,9 +1,13 @@
 import { Menu } from "lucide-react"
 
-export default function DropdownMenuButton() {
+type Props = {
+  onClick: () => void
+}
+
+export default function DropdownMenuButton(props: Props) {
   return (
-    <div className="dropdown-menu-btn md:hidden">
-      <Menu size={35} />
+    <div className="dropdown-menu-btn md:hidden cursor-pointer" onClick={props.onClick}>
+      <Menu color="#181A20" size={35} />
     </div>
   )
 }
