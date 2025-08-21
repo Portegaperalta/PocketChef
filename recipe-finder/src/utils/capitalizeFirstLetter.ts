8f5aa27c05fe36297162e8c88df3e59a86b342e8
Pablo <ortegaperaltap@gmail.com>
2025-08-21 00:53:01 -0400

    add error handling to capitalizeFirstLetter function

--- recipe-finder/src/utils/capitalizeFirstLetter.ts
+++ recipe-finder/src/utils/capitalizeFirstLetter.ts
@@ -1,7 +1,12 @@
 export default function capitalizeFirstLetter(text: string) {
-  if (text.length === 0) {
-    return ""
-  } else {
-    return text.charAt(0).toUpperCase() + text.slice(1)
+  try {
+    if (text.length === 0) {
+      return ""
+    } else {
+      return text.charAt(0).toUpperCase() + text.slice(1)
+    }
+  }
+  catch (error) {
+    console.log(error)
   }
 }
