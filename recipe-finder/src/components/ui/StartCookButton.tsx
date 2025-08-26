3ab5950cac2da2837f115949e5d74c1ef1ff0bde
Pablo <ortegaperaltap@gmail.com>
2025-08-26 13:50:38 -0400

    add hover animation to startCookButton component

--- recipe-finder/src/components/ui/StartCookButton.tsx
+++ recipe-finder/src/components/ui/StartCookButton.tsx
@@ -1,15 +1,20 @@
 import { ChefHat } from 'lucide-react';
+import { motion } from 'motion/react'
 
 export default function StartCookButton() {
   return (
-    <div className="start-cook-btn group bg-(--clr-primary) py-1.5 px-2 
-    flex flex-row items-center gap-1 rounded-lg hover:bg-(--clr-primary-hover) 
-    duration-200 ease-in-out cursor-pointer">
-      <ChefHat color='#FFFF'
-        className='hidden group-hover:inline-block duration-200 ease-in-out' />
-      <p className="text-(--clr-white) font-[500]">
-        Start Cook
-      </p>
-    </div>
+    <motion.div
+      whileHover={{ scale: 1.1 }}
+    >
+      <div className="start-cook-btn group bg-(--clr-primary) py-1.5 px-2 
+      flex flex-row items-center gap-1 rounded-lg hover:bg-(--clr-primary-hover) 
+      duration-200 ease-in-out cursor-pointer">
+        <ChefHat color='#FFFF'
+          className='hidden group-hover:inline-block duration-200 ease-in-out' />
+        <p className="text-(--clr-white) font-[500]">
+          Start Cook
+        </p>
+      </div>
+    </motion.div>
   )
 }
