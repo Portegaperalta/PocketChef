a75cd318a40f6d9f7bc5dd2ad78916a073862aa6
Pablo <ortegaperaltap@gmail.com>
2025-08-31 15:00:23 -0400

    add while tap effects to sign in and sign up buttons

--- recipe-finder/src/components/ui/SignInFormButton.tsx
+++ recipe-finder/src/components/ui/SignInFormButton.tsx
@@ -1,15 +1,17 @@
 import { Button } from "./button";
-
+import { motion } from 'motion/react'
 
 export default function SignInFormButton() {
   return (
-    <Button type='submit'
-      id="submitFormButton"
-      className='text-(--clr-secondary) text-[1.1rem] font-[500] 
+    <motion.div whileTap={{ scale: 0.9 }}>
+      <Button type='submit'
+        id="submitFormButton"
+        className='text-(--clr-secondary) text-[1.1rem] font-[500] 
       px-4 bg-(--clr-primary) cursor-pointer hover:text-(--clr-white) 
       hover:bg-(--clr-primary)'
-    >
-      Sign In
-    </Button>
+      >
+        Sign In
+      </Button>
+    </motion.div>
   )
 }
--- recipe-finder/src/components/ui/SignUpFormButton.tsx
+++ recipe-finder/src/components/ui/SignUpFormButton.tsx
@@ -1,13 +1,16 @@
 import { Button } from "./button"
+import { motion } from 'motion/react'
 
 export default function SignUpFormButton() {
   return (
-    <Button id="submitFormButton" type='submit'
-      className='text-(--clr-secondary) text-[1.1rem] font-[500] 
+    <motion.div whileTap={{ scale: 0.9 }}>
+      <Button id="submitFormButton" type='submit'
+        className='text-(--clr-secondary) text-[1.1rem] font-[500] 
       px-4 bg-(--clr-primary) cursor-pointer hover:text-(--clr-white) 
       hover:bg-(--clr-primary)'
-    >
-      Sign Up
-    </Button>
+      >
+        Sign Up
+      </Button>
+    </motion.div>
   )
 }
