85438b8dc7e323f66c6fdd0f13b076c3154aa95c
Pablo <ortegaperaltap@gmail.com>
2025-08-31 11:50:25 -0400

    remove padding and border styles to login form inputs and adding them to the parent div instead; add show and hide password buttons *no functionality yet*

--- recipe-finder/src/pages/Login/LoginForm.tsx
+++ recipe-finder/src/pages/Login/LoginForm.tsx
@@ -1,26 +1,35 @@
 import { Eye } from 'lucide-react';
+import { EyeOff } from 'lucide-react';
 
 export default function LoginForm() {
   return (
     <section className="login-form mt-8">
       <form id="loginForm" className="flex flex-col gap-4">
-        <div className="username-input">
+        <div className="username-input py-2 px-4 flex flex-row 
+        items-center border-2 border-(--clr-secondary) rounded-md">
           <input type="text"
             id="username"
             placeholder="Username"
             autoComplete="off"
-            className="text-(--clr-secondary) text-[1.1rem] border-2 border-(--clr-secondary) 
-            py-2 px-2 rounded-md outline-none w-full"
+            className="text-(--clr-secondary) text-[1.1rem] font-[500]
+            outline-none w-full"
           />
         </div>
-        <div className="password-input">
+        <div className="password-input py-2 px-2 flex flex-row 
+        items-center border-2 border-(--clr-secondary) rounded-md">
           <input type="password"
             id="userPassword"
             placeholder="Password"
             autoComplete="off"
-            className="text-(--clr-secondary) text-[1.1rem] border-2 border-(--clr-secondary) 
-            py-2 px-2 rounded-md outline-none w-full"
+            className="text-(--clr-secondary) text-[1.1rem] font-[500] 
+            outline-none w-full"
           />
+          <div className="show-password-btn">
+            <Eye />
+          </div>
+          <div className="hide-password-btn hidden">
+            <EyeOff />
+          </div>
         </div>
       </form>
     </section>
