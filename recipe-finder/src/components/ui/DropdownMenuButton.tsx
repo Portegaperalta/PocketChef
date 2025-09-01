0ae3a901dd12bf3e1de9d9a9e255de658685d1bd
Pablo <ortegaperaltap@gmail.com>
2025-08-31 20:17:22 -0400

    add rotation animation to  dropdownMenuButton on click

--- recipe-finder/src/components/ui/DropdownMenuButton.tsx
+++ recipe-finder/src/components/ui/DropdownMenuButton.tsx
@@ -1,13 +1,26 @@
 import { Menu } from "lucide-react"
+import { motion } from 'motion/react'
+import { useState } from "react"
 
 type Props = {
   onClick: () => void
 }
 
 export default function DropdownMenuButton(props: Props) {
+  const [buttonRotation, setButtonRotation] = useState(0)
+
+  const handleButtonClick = () => {
+    setButtonRotation(prev => prev + 90)
+  }
+
   return (
-    <div className="dropdown-menu-btn md:hidden cursor-pointer" onClick={props.onClick}>
-      <Menu color="#181A20" size={35} />
-    </div>
+    <motion.button
+      animate={{ rotate: buttonRotation }}
+      onClick={handleButtonClick}
+    >
+      <div className="dropdown-menu-btn md:hidden cursor-pointer" onClick={props.onClick}>
+        <Menu color="#181A20" size={35} />
+      </div>
+    </motion.button>
   )
 }
