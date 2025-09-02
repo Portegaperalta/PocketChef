9609644346f7f1a24085cfe96a5ca036194e5776
Pablo <ortegaperaltap@gmail.com>
2025-09-02 13:41:46 -0400

    create function to check if recipe is saved and update isRecipeSaved state; change Save RecipeButton color if the recipe is already saved

--- recipe-finder/src/components/ui/SaveRecipeButton.tsx
+++ recipe-finder/src/components/ui/SaveRecipeButton.tsx
@@ -1,8 +1,17 @@
 import FetchRecipeById from '@/api/FetchRecipeById';
 import { Bookmark } from 'lucide-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { toast } from 'sonner';
 import { motion } from 'motion/react'
+
+type Recipe = {
+  id: number,
+  title: string,
+  image: string,
+  spoonacularScore: number,
+  ingredientsSumary?: string,
+  BenefitSummary?: string,
+}
 
 type Props = {
   id: number
@@ -13,6 +22,16 @@ export default function SaveRecipeButton(props: Props) {
   const [buttonBackground, setButtonBackground] = useState(false)
 
   const savedRecipes = JSON.parse(sessionStorage.getItem('savedRecipes') || "[]")
+
+  const checkIfRecipeIsSaved = () => {
+    const recipeIsSaved = savedRecipes.some(savedRecipe => savedRecipe.id === props.id)
+
+    if (recipeIsSaved === true) {
+      setIsRecipeSaved(true)
+    } else {
+      setIsRecipeSaved(false)
+    }
+  }
 
   const saveRecipe = async () => {
     const recipeData = await FetchRecipeById(props.id)
@@ -28,13 +47,20 @@ export default function SaveRecipeButton(props: Props) {
     toast("Recipe saved")
   }
 
+  useEffect(() => {
+    checkIfRecipeIsSaved()
+  }, [])
+
   return (
     <motion.div whileTap={{ scale: 0.9 }}>
       <div
         onClick={HandleButtonClick}
         title='Save recipe'
         className={`save-recipe-button cursor-pointer`}>
-        <Bookmark color='#F6B100' fill={buttonBackground ? `#F6B100` : `transparent`} />
+        <Bookmark
+          color='#F6B100'
+          fill={(buttonBackground || isRecipeSaved) ? `#F6B100` : `transparent`}
+        />
       </div>
     </motion.div>
   )
