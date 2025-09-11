9b405e02344381dca9eb0d59823fd7de24b3963c
Pablo <ortegaperaltap@gmail.com>
2025-09-11 00:21:00 -0400

    add type to app and auth variables in firebase config.ts

--- recipe-finder/src/firebase/config.ts
+++ recipe-finder/src/firebase/config.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth'
+import { initializeApp, type FirebaseApp } from "firebase/app";
+import { getAuth, type Auth } from 'firebase/auth'
 import { getAnalytics } from "firebase/analytics";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -19,8 +19,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app: FirebaseApp = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export const auth = getAuth(app)
+export const auth: Auth = getAuth(app)
 export default app
