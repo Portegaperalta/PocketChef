6533ae1a2f6efc79c551be3b2cbd915abe8ac852
Pablo <ortegaperaltap@gmail.com>
2025-09-11 23:30:54 -0400

    wrap app with AuthProvider

--- recipe-finder/src/main.tsx
+++ recipe-finder/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext.tsx'
 import './index.css'
 import App from './App.tsx'
 
@@ -8,7 +9,9 @@ import App from './App.tsx'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
