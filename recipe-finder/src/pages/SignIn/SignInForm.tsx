03995790a0a14a579404ebb56190e609857935f1
Pablo <ortegaperaltap@gmail.com>
2025-09-12 00:17:34 -0400

    start signInForm refactor

--- recipe-finder/src/pages/SignIn/SignInForm.tsx
+++ recipe-finder/src/pages/SignIn/SignInForm.tsx
@@ -4,75 +4,55 @@ import { FirebaseError } from 'firebase/app';
 import SignInFormButton from '@/components/ui/SignInFormButton';
 import { Eye } from 'lucide-react';
 import { EyeOff } from 'lucide-react';
+import { useNavigate } from 'react-router';
 
-export default function LoginForm() {
-  const [_passwordInput, setPasswordInput] = useState("")
-  const [isPasswordVisible, SetIsPasswordVisible] = useState(false)
+export default function LoginForm(): React.FC {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [error, setError] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+  const { login } = useAuth();
+  const navigate = useNavigate();
 
-  const updatePasswordInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPasswordInput(e.target.value)
-  }
-
-  const showPassword = () => {
-    SetIsPasswordVisible(true)
-  }
-
-  const hidePassword = () => {
-    SetIsPasswordVisible(false)
-  }
-
-  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  async function handleFormSubmit(e: React.FormEvent<HTMLFormElement>): {
     e.preventDefault()
-  }
+}
 
-  return (
-    <section className="login-form mt-8 w-full">
-      <form
-        id="signInForm"
-        action="#"
-        onSubmit={handleFormSubmit}
-        className="flex flex-col gap-6 items-center">
-        <div className="username-input py-2 px-4 flex flex-row 
+return (
+  <section className="login-form mt-8 w-full">
+    <form
+      id="signInForm"
+      action="#"
+      onSubmit={handleFormSubmit}
+      className="flex flex-col gap-6 items-center">
+      <div className="username-input py-2 px-4 flex flex-row 
          items-center border-2 border-(--clr-secondary) rounded-md
          w-full max-w-90 lg:max-w-100">
-          <input type="text"
-            id="username"
-            placeholder="Username"
-            autoComplete="off"
-            required
-            className="text-(--clr-secondary) text-[1.1rem] font-[500]
+        <input type="text"
+          id="username"
+          placeholder="Username"
+          autoComplete="off"
+          required
+          className="text-(--clr-secondary) text-[1.1rem] font-[500]
             outline-none w-full"
-          />
-        </div>
-        <div className="password-input py-2 px-2 flex flex-row 
+        />
+      </div>
+      <div className="password-input py-2 px-2 flex flex-row 
          items-center border-2 border-(--clr-secondary) rounded-md 
          w-full max-w-90 lg:max-w-100">
-          <input type={`${isPasswordVisible ? `text` : `password`}`}
-            id="userPassword"
-            placeholder="Password"
-            autoComplete="off"
-            required
-            onChange={updatePasswordInput}
-            className="text-(--clr-secondary) text-[1.1rem] font-[500] 
+        <input type={`password`}
+          id="userPassword"
+          placeholder="Password"
+          autoComplete="off"
+          required
+          className="text-(--clr-secondary) text-[1.1rem] font-[500] 
             outline-none w-full"
-          />
-          <div
-            onClick={showPassword}
-            className={`show-password-btn ${isPasswordVisible ? `hidden` : `inline-block`} 
-            cursor-pointer`}>
-            <Eye />
-          </div>
-          <div
-            onClick={hidePassword}
-            className={`hide-password-btn hidden ${isPasswordVisible ? `inline-block` : `hidden`} 
-            cursor-pointer`}>
-            <EyeOff />
-          </div>
-        </div>
-        <div className="submit-form-button text-center">
-          <SignInFormButton />
-        </div>
-      </form>
-    </section>
-  )
+        />
+      </div>
+      <div className="submit-form-button text-center">
+        <SignInFormButton />
+      </div>
+    </form>
+  </section>
+)
 }
