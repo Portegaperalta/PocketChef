df25156c2696585b3fe64342f5ce9d12374c8197
Pablo <ortegaperaltap@gmail.com>
2025-09-13 13:56:37 -0400

    add position fixed to MobileBar and add white background

--- recipe-finder/src/components/MobileBar.tsx
+++ recipe-finder/src/components/MobileBar.tsx
@@ -11,28 +11,29 @@ const navItems: NavItem[] = [
   {
     name: "Home",
     href: "/",
-    icon: <Home color="#9F9F9F" size={30} />,
+    icon: <Home color="#9F9F9F" size={28} />,
   },
   {
     name: "Search-Recipe",
     href: "/Search-Recipe",
-    icon: <Search color="#9F9F9F" size={30} />,
+    icon: <Search color="#9F9F9F" size={28} />,
   },
   {
     name: "Saved Recipes",
     href: "/Saved-Recipes",
-    icon: <Bookmark color="#9F9F9F" size={30} />,
+    icon: <Bookmark color="#9F9F9F" size={28} />,
   },
   {
     name: "Sign In",
     href: "/SignIn",
-    icon: <UserRound color="#9F9F9F" size={30} />,
+    icon: <UserRound color="#9F9F9F" size={28} />,
   },
 ]
 
 export default function MobileBar() {
   return (
-    <div className="mobile-navbar px-6 py-4 border-t-1 md:hidden">
+    <div className="mobile-navbar px-6 py-4 fixed bottom-0 border-t-1
+     w-full z-100 bg-(--clr-white) md:hidden">
       <nav>
         <ul className="flex flex-row justify-between">
           {
